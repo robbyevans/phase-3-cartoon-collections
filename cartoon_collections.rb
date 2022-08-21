820c9e546a5d33e59440383bc4dd20620d596a63
Evans Rutto <robbyevans001@gmail.com>
2022-08-21 13:04:31 +0300

    commit tree 138b5c8c07d69c51c642ce673b3a387e12b1ffad

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each do |dwaf|
+    puts "#{array.find_index(dwaf)+1}. #{dwaf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array=[]
+  array.each do |word|
+   new_array<<"#{word.capitalize}!"
+  end 
+  return new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.length<=4
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  array.each do |item|
+    case item
+    when "cheddar"
+      puts 'cheddar'
+    when "gouda"
+      return "gouda"
+    when "camembert"
+      return "camembert"
+    else
+      return nil
+    end
+  end
 end
+
+pp find_the_cheese(cheese_types)
